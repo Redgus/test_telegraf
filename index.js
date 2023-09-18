f82dd862bb2a17def6814f02967b5d9dc64e9c78
redgus <vadimdibcov@gmail.com>
2023-09-18 16:49:57 +0500

    add loggin

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const config = require('config');
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(config.get('token'))
+
+bot.use(async (ctx, next) => {
+
+    const date = new Date();
+
+    console.log(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] Пользователь ${ctx.from.id}`);
+    await next();
+});
+
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.catch((err, ctx) => {
+    console.error(`⚠️ Error on ${ctx.updateType}: ${err.message}`, err)
+});
+
 bot.launch()
