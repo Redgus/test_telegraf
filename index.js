ff467cc6499ec932d32364a0a5f0562f6845257f
redgus <vadimdibcov@gmail.com>
2023-09-20 16:20:37 +0500

    add functional

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const config = require('config');
 const { Telegraf } = require('telegraf')
 const { MongoClient } = require('mongodb');
 const { session } = require('telegraf-session-mongodb');
+const axios = require('axios');
 
 const bot = new Telegraf(config.get('token'))
 
 const init = async () => {
-    const db = (await MongoClient.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true, useUnifiedTopology: true, dbName : 'telegram' })).db();
+    const db = (await MongoClient.connect(config.get('Mongo.url'), { useNewUrlParser: true, useUnifiedTopology: true, dbName : 'telegram' })).db();
 
     bot.use(session(db));
 
@@ -27,6 +28,32 @@ const init = async () => {
     bot.on('sticker', (ctx) => ctx.reply('👍'))
     bot.hears('hi', (ctx) => ctx.reply('Hey there'))
     
+    bot.command('get_rate', (ctx) => {
+
+        let mas = ['RUB', 'EUR', 'USD'];
+
+        let date = new Date();
+
+        for (const key of mas) {
+            axios.get(`${config.get('url_rate')}/${key}/${date.getUTCFullYear()}-0${date.getUTCMonth() + 1}-${date.getUTCDate()}`)
+            .then(function (response) {
+
+                for (const iterator of response.data) {
+                    ctx.reply(`${iterator.CcyNm_UZ} - ${iterator.Rate} - ${ iterator.Date }`)
+                }
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+        }
+
+        
+    })
+
     bot.catch((err, ctx) => {
         console.error(`⚠️ Error on ${ctx.updateType}: ${err.message}`, err)
     });
