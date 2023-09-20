740215cacabba0454e0583e31c45bee9f3d02e26
redgus <vadimdibcov@gmail.com>
2023-09-20 13:30:17 +0500

    add mongo

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const config = require('config');
 const { Telegraf } = require('telegraf')
+const { MongoClient } = require('mongodb');
+const { session } = require('telegraf-session-mongodb');
 
 const bot = new Telegraf(config.get('token'))
 
-bot.use(async (ctx, next) => {
+const init = async () => {
+    const db = (await MongoClient.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true, useUnifiedTopology: true, dbName : 'telegram' })).db();
 
-    const date = new Date();
+    bot.use(session(db));
 
-    console.log('new code');
-    console.log(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] Пользователь ${ctx.from.id}`);
-    await next();
-});
+    bot.use(async (ctx, next) => {
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+        const date = new Date();
+    
+        console.log('new code');
+    
+        ctx.session.date = `[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`;
+        
+        console.log(`[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] Пользователь ${ctx.from.id}`);
+        await next();
+    });
+    
+    bot.start((ctx) => ctx.reply('Welcome'))
+    bot.help((ctx) => ctx.reply('Send me a sticker'))
+    bot.on('sticker', (ctx) => ctx.reply('👍'))
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+    
+    bot.catch((err, ctx) => {
+        console.error(`⚠️ Error on ${ctx.updateType}: ${err.message}`, err)
+    });
+	
+    bot.launch();
+}
 
-bot.catch((err, ctx) => {
-    console.error(`⚠️ Error on ${ctx.updateType}: ${err.message}`, err)
-});
-
-bot.launch()
+init()
